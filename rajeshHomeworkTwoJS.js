1cf65d4aef816fe1406bb1e3101b3ec6751b58da
Rajesh Chikatla <108161887+rchikatla100@users.noreply.github.com>
2025-10-23 23:38:57 -0500

    Implement validation functions for user input
    Added validation functions for first name, last name, phone number, username, and password. Improved error handling and user feedback.

--- rajeshHomeworkTwoJS.js
+++ rajeshHomeworkTwoJS.js
@@ -18,7 +18,42 @@ output.innerHTML = slider.value;
     output.innerHTML = this.value;
   }
 
+//validate first name
+function validateFirstName()
+{
+  let firstName = document.getElementById("firstname").value;
+  let nameRegularExpression = /^[A-Za-z'-]+$/;
 
+  if (!nameRegularExpression.test(firstName))
+  {
+    document.getElementById("firstNameError").innerHTML = 
+    "First name can contain only letters, apostrophes, or hyphens.";
+    return false;
+  }
+  else 
+  {
+    document.getElementById("firstNameError").innerHTML = "";
+    return true;
+  }
+}
+// Validate last name
+function validateLastName()
+{
+  let lastName = document.getElementById("lastname").value;
+  let nameRegularExpression = /^[a-zA-Z'2-5\s-.]+$/;
+
+  if (!nameRegularExpression.test(lastName))
+  {
+    document.getElementById("lastNameError").innerHTML = 
+    "Last name can only contain letters, apostrophes, dashes, and suffixes ie. IV, 2nd.";
+    return false;
+  }
+  else 
+  {
+    document.getElementById("lastNameError").innerHTML = "";
+    return true;
+  }
+}
 
 // Validate date of birth
 function validateDOB()
@@ -112,24 +147,26 @@ function validateEmail()
     return true;
   }
 }
-
+//Validate Phone Number
 function validatePhone()
 {
-  let phone = document.getElementById("phone");
+  phone = document.getElementById("phone").value;
   var phoneRegularExpression = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
 
-  if (phone.length !== 12)
+  if (phone.length != 12)
   {
     document.getElementById("phoneError").innerHTML = 
     "Please enter a 9-digit phone number (###-###-####).";
     return false;
   }
+  /*
   else if (!phone.match(phoneRegularExpression))
   {
     document.getElementById("phoneError").innerHTML = 
     "Please enter a valid phone number (###-###-####).";
     return false;
   }
+  */
   else 
   {
     document.getElementById("phoneError").innerHTML = "";
@@ -147,6 +184,14 @@ function validateUsername()
   username = username.toLowerCase();
   document.getElementById("username").value = username;
 
+  if (username.length == 0)
+  {
+    document.getElementById("usernameError").innerHTML = 
+    "Username field cannot be empty.";
+    return false; 
+  }
+
+
   if (!isNaN(username.charAt(0)))
   {
     document.getElementById("usernameError").innerHTML = 
@@ -154,35 +199,25 @@ function validateUsername()
     return false;
   }
 
-  if (username.length == 0)
-  {
-    document.getElementById("usernameError").innerHTML = 
-    "Username cannot be blank.";
-    return false; 
-  }
-
-  let usernameRegularExpression = /^[A-Za-z0-9_]{5,20}$/;
+  let usernameRegularExpression = /^[A-Za-z0-9_]+$/;
   if (!usernameRegularExpression.test(username))
   {
     document.getElementById("usernameError").innerHTML = 
-    "Username must be at least 5 characters long and contain only letters, numbers, or underscore.";
+    "Username can contain only letters, numbers, or underscore.";
     return false;
   }
-
-  else if (username.length > 30)
-  {
-    document.getElementById("usernameError").innerHTML = 
-    "Username cannot exceed 30 characters.";
-    return false;
-  } 
-
   else if (username.length < 5)
   {
     document.getElementById("usernameError").innerHTML = 
     "Username must be at least 5 characters long.";
     return false;
   }
-
+  else if (username.length > 30)
+  {
+    document.getElementById("usernameError").innerHTML = 
+    "Username cannot exceed 30 characters.";
+    return false;
+  } 
   else 
   {
     document.getElementById("usernameError").innerHTML = "";
@@ -195,46 +230,48 @@ function validateUsername()
 // Validate Password
 function validatePassword()
 {
-  let password = document.getElementById("password").value;
-  let username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
 
-  const passwordError = [];
+  const errorMessage = [];
 
   if (!password.match(/[a-z]/)) // lowercase letter
   {
-    passwordError.push("Please enter at least one lowercase letter.");
+    errorMessage.push("Please enter at least one lowercase letter.");
   }
+  
   if (!password.match(/[A-Z]/)) // uppercase letter
   {
-    passwordError.push("Please enter at least one uppercase letter.");
+    errorMessage.push("Please enter at least one uppercase letter.");
   }
+
   if (!password.match(/[0-9]/)) // at least one number
   {
-    passwordError.push("Please enter at least one number.");
+    errorMessage.push("Please enter at least one number.");
   }
+
   if (!password.match(/[!@#%^&*()-_+=\/><.,`~]/)) // special character
   {
-    passwordError.push("Please enter at least one special character (!@#%^&*()-_+=\/><.,`~).");
+    errorMessage.push("Please enter at least one special character (!@#%^&*()-_+=\/><.,`~).");
   }
 
   if (password == username || password.includes(username))  // password can't contain username
   {
-    passwordError.push("Password cannot contain username.");
+    errorMessage.push("Password cannot contain username.");
   }
 
-  const errorContainer = document.querySelector(".passwordMessage");
-  errorContainer.innerHTML = passwordError
-  .map((message) => `<span>{$message}</span><br/>`)
+  const errorContainer = document.querySelector(".passwordError");
+  errorContainer.innerHTML = errorMessage
+  .map((message) => `<span>${message}</span><br/>`)
   .join("");
 }
-
 
 // Validate confirm password
 
 function confirmPassword()
 {
-  password1 = document.getElementById("password").value;
-  password2 = document.getElementById("confirmpassword").value;
+  const password1 = document.getElementById("password").value;
+  const password2 = document.getElementById("confirmpassword").value;
 
   if (password1 !== password2)
   {
@@ -247,6 +284,66 @@ function confirmPassword()
     document.getElementById("confirmPasswordError").innerHTML = "Passwords match!";
     return true;
   }
+}
+
+
+
+// Review button
+function reviewInput()
+{
+  var formcontent = document.getElementById("signup");
+  var formoutput;
+  var datatype;
+  var i;
+  formoutput = "<table class = 'output'><th colspan  = '3'> Please review your input: </th>";
+  for (i = 0; i < formcontent.length; i++)
+    if (formcontent.elements[i].value != "")
+    {
+      datatype = formcontent.elements[i].type;
+      switch (datatype)
+      {
+        case "checkbox": //checkbox elements
+          if (formcontent.elements[i].checked)
+          {
+            formoutput = formoutput + "<tr> <td align = 'right'>" + formcontent.elements[i].name + "</td>";
+            formoutput = formoutput + "<td class = 'outputdata'></td></tr>"
+          }
+          break;
+        case "radio": //radio button elements
+          if (formcontent.elements[i].checked)
+          {
+            formoutput = formoutput + "<tr> <td align = 'right'>" + formcontent.elements[i].name + "</td>"; 
+            formoutput = "<td class = 'outputdata'>" + formcontent.elements[i].value + "</td></tr>";
+          }
+          break;
+
+        case "submit": 
+        case "button": 
+        case "reset":
+        
+          formoutput = formoutput + "<tr><td align = 'right'>" +
+          formcontent.elements[i].name + "</td>";
+          formoutput = formoutput + "<td class = 'outputdata'>" +
+          formcontent.elements[i].value + "</td></tr>";
+          break;
+          default:
+          formoutput = formoutput + "<tr><td align = 'right'>" +
+          formcontent.elements[i].name + "</td>";
+          formoutput = formoutput + "<td class = 'outputdata'>" +
+          formcontent.elements[i].value + "</td></tr>";
+      }
+    }
+
+    if (formoutput.length > 0)
+    {
+      formoutput = formoutput + "</table>";
+      document.getElementById("showInput").innerHTML = formoutput;
+    }
+  }
+
+function removeReview()
+{
+  document.getElementById("showInput").innerHTML = "";
 }
 
 
